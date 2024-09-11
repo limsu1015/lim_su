fe813d34b52ccfbc3b0cfb5cf84a58c8c8bf7d56
limsu1015 <tngur1015@gmail.com>
2024-09-11 17:34:52 +0900

    python 과제

---
+++ fish.py
@@ -0,0 +1,22 @@
+"""
+
+"""
+# 붕어빵 재고 데이터 생성
+stock = {
+    "팥붕어빵" : 10, 
+    "슈크림붕어빵" : 8, 
+    "초코붕어빵" : 5
+}
+
+#len(stock) : 함수  stock.items()
+for bread, quantity in stock.items():
+    print(f'{bread} : {quantity}')
+
+
+
+
+
+
+
+
+#print(stock)
