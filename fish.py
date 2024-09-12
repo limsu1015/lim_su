333c4db0b4a47ad6d26fe0a6dbb7d9730d1f65be
limsu1015 <tngur1015@gmail.com>
2024-09-12 17:16:59 +0900

    <기능개발> 붕어빵 주문 처리
    손님 주문 내역에 따라 재고 관리 기능 구현
    손님이 제고보다 많은 양의 빵을주문 할 경우 판매 되지 않고 재고가 부족하다는 알림메세지 출력 기능 구

--- fish.py
+++ fish.py
@@ -13,13 +13,13 @@ for bread, quantity in stock.items():
     print(f'{bread} : {quantity}')
 
 # 1. 한번에 맛과 개수를 받는거
-order = {}
-bread_kind, bread_count = input("주문 할 붕어빵의 종류와 갯수를 입력해 주세요").split()
-order[bread_kind] = bread_count + "개"
-print(f'주문내역: {order}')
+# order = {}
+# bread_kind, bread_count = input("주문 할 붕어빵의 종류와 갯수를 입력해 주세요").split()
+# order[bread_kind] = bread_count + "개"
+# print(f'주문내역: {order}')
 # 2. map함수를 이용해서 bead_count 코드를 개선
 # bread_count = map(int, input('주문 할 갯수를 입력해주세요'))
-"""order = {}
+order = {}
 
 
 bread_kind = input("주문할 붕어빵 종류를 입력하세요 (팥붕어빵, 슈크림붕어빵, 초코붕어빵)")
@@ -27,8 +27,16 @@ bread_count = int(input("주문 할 갯수를 입력해주세요"))
 
 order[bread_kind] = bread_count
 
-print(f'주문내역: {order}')"""
+print(f'주문내역: {order}')
 
+
+if stock[bread_kind] >= bread_count:
+    stock[bread_kind] -= bread_count
+    print(f'{bread_kind}: {bread_count}개를 판매히였습니다')
+else:
+    result = bread_count - stock[bread_kind]
+    print(f"{bread_kind}재고가 {result}개 부족합니다")
+    
 
 
 #print(stock)
