8ce5e3ee9fe1f2e8b68bd9f0bf6556f8c5deb40b
limsu1015 <tngur1015@gmail.com>
2024-09-12 17:46:46 +0900

    <기능개발> 종료 메세지 입력 전까지 작동하도록 개선 종료입력시 프로그램 종료

--- fish.py
+++ fish.py
@@ -7,24 +7,19 @@ stock = {
     "슈크림붕어빵" : 8, 
     "초코붕어빵" : 5
 }
-
+"""
 #len(stock) : 함수  stock.items()
 for bread, quantity in stock.items():
     print(f'{bread} : {quantity}')
 
 # 1. 한번에 맛과 개수를 받는거
 # order = {}
-# bread_kind, bread_count = input("주문 할 붕어빵의 종류와 갯수를 입력해 주세요").split()
-# order[bread_kind] = bread_count + "개"
-# print(f'주문내역: {order}')
-# 2. map함수를 이용해서 bead_count 코드를 개선
-# bread_count = map(int, input('주문 할 갯수를 입력해주세요'))
+ bread_count = map(int, input('주문 할 갯수를 입력해주세요'))
 order = {}
 
 
 bread_kind = input("주문할 붕어빵 종류를 입력하세요 (팥붕어빵, 슈크림붕어빵, 초코붕어빵)")
 bread_count = int(input("주문 할 갯수를 입력해주세요"))
-
 order[bread_kind] = bread_count
 
 print(f'주문내역: {order}')
@@ -37,6 +32,37 @@ else:
     result = bread_count - stock[bread_kind]
     print(f"{bread_kind}재고가 {result}개 부족합니다")
     
+
+for bread, quantity in stock.items():
+    print(f'{bread} : {quantity}')# bread_kind, bread_count = input("주문 할 붕어빵의 종류와 갯수를 입력해 주세요").split()
+# order[bread_kind] = bread_count + "개"
+# print(f'주문내역: {order}')
+# 2. map함수를 이용해서 bead_count 코드를 개선
+"""
+
+
+while True:
+    order = {}
+    bread_kind = input("주문할 붕어빵 종류를 입력하세요 (팥붕어빵, 슈크림붕어빵, 초코붕어빵) 종료를 원하는 경우 '종료'를 입력해 주세요")
+    if bread_kind == "종료":
+        break
+    
+    bread_count = int(input("주문 할 갯수를 입력해주세요"))
+
+    if stock[bread_kind] >= bread_count:
+        stock[bread_kind] -= bread_count
+        print(f'{bread_kind}: {bread_count}개를 판매히였습니다')
+    else:
+        result = bread_count - stock[bread_kind]
+        print(f"{bread_kind}재고가 {result}개 부족합니다")
+
+
+    for bread, quantity in stock.items():
+        print(f'{bread} : {quantity}')
+
+
+
+
 
 
 #print(stock)
